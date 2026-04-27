b99a2d4497afb24d38284605373db28a2b7ab9cd
gxydevbot <gxydevbot@users.noreply.github.com>
2026-04-27 06:24:28 +0000

    deploy: f8138af835edca2ee672498d6824c7ebac88c16f

---
+++ _astro/chunk-FMBD7UC4.DNAHue7a.js
@@ -0,0 +1,15 @@
+import{_ as o}from"./MarkdownRenderer.B0mWlil8.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"f8138af835edca2ee672498d6824c7ebac88c16f"};var n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="0557e113-ef40-4a33-910c-7deb304aa017",e._sentryDebugIdIdentifier="sentry-dbid-0557e113-ef40-4a33-910c-7deb304aa017")}catch{}})();var a=o(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{a as g};
